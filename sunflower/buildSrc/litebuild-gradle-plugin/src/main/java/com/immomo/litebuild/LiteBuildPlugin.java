7a081c9de80c88882caa9c3a4c0a2b00feb188f8
zhouhuacong <zhou.huacong@immomo.com>
2021-06-01 17:15:31 +0800

    升级lib和插件版本号

--- sunflower/buildSrc/litebuild-gradle-plugin/src/main/java/com/immomo/litebuild/LiteBuildPlugin.java
+++ sunflower/buildSrc/litebuild-gradle-plugin/src/main/java/com/immomo/litebuild/LiteBuildPlugin.java
@@ -83,7 +83,7 @@ public class LiteBuildPlugin implements Plugin<Project> {
 
         if (!project.getGroup().equals("sunflower")) {
             project.getDependencies().add("implementation",
-                    project.getDependencies().create("com.immomo.litebuild:build-lib:0.0.603-SNAPSHOT"));
+                    project.getDependencies().create("com.immomo.litebuild:build-lib:0.0.1"));
         }
     }
 
