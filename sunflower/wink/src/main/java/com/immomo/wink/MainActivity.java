7d5733e0cb7b7d0c25eaa799cef09624f8ce3ba0
zhouhuacong <zhou.huacong@immomo.com>
2021-06-03 12:00:28 +0800

    提交wink gradle 5.4.1工程

---
+++ sunflower/wink/src/main/java/com/immomo/wink/MainActivity.java
@@ -0,0 +1,16 @@
+package com.immomo.wink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("zhc", "Asdadsadasdsadfsafffffdsaf");
+    }
+}
